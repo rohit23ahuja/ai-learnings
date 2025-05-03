9054ab6a599fd52688f6ac2c06b8f695dc61f448
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-05-03 07:15:25 +0530

    Updating script

--- openai-api-call.sh
+++ openai-api-call.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Calling Open AI..."
-MY_OPENAI_KEY=""
+MY_OPENAI_KEY=$OPENAI_API_KEY
 PROMPT="Write a three-sentence story about sikh guru Guru Nanak."
 
 curl https://api.openai.com/v1/responses \
